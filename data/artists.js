5c012563c238bab4e1ab3841e566ef2ddaed5ced
duerain3630 <147635522+duerain3630@users.noreply.github.com>
2025-12-21 22:48:28 -0300

    Add ARTISTS data structure with artist details

---
+++ data/artists.js
@@ -0,0 +1,19 @@
+const ARTISTS = {
+  magdalena_bay: {
+    name: "Magdalena Bay",
+    logo: "images/magdalena_bay/magdalena-bay-logo.png",
+    songs: [
+      { title: "Secrets (Your Fire)", image: "images/magdalena_bay/secrets.jpg" },
+      { title: "Killshot", image: "images/magdalena_bay/killshot.jpg" },
+    ]
+  },
+
+  ninos_del_cerro: {
+    name: "Niños del Cerro",
+    logo: "images/ninos_del_cerro/logo.jpg",
+    songs: [
+      { title: "Nonato Coo", image: "images/ninos_del_cerro/nonato.jpg" },
+      { title: "Flores, labios, dedos", image: "images/ninos_del_cerro/flores.jpg" },
+    ]
+  }
+};
